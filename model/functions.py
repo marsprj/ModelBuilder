d588dc0a99a7cde7edd0498fe41ed0e2c2299843
fanzia <caisefan@163.com>
2017-09-19 18:55:54 -0700

    增加平滑滤波算法

--- model/functions.py
+++ model/functions.py
@@ -257,9 +257,9 @@ def process_meanimagefilter(func, process, user_uuid):
     return True
 
 """
-处理中值滤波
+处理平滑濾波
 """
-def process_medianimagefilter(func, process, user_uuid):
+def process_smoothing(func, process, user_uuid):
     try:
         task_id = str(process.task_id)
         parms = func.getParms()
@@ -288,8 +288,50 @@ def process_medianimagefilter(func, process, user_uuid):
                 command = "{0} {1} {2}".format(command, key, res)
 
 
-        command = "{0} {1}".format("MedianImageFilter", command)
+        command = "{0} {1}".format("otbcli_Smoothing", command)
         doFunction(process, command)
+    except Exception as e:
+        logger.error("process run failed: {0}".format(str(e)))
+        raise e
+        return False
+    return True
+
+"""
+处理边缘检测
+"""
+def process_edgeextraction(func,process,user_uuid):
+    try:
+        task_id = str(process.task_id)
+        parms = func.getParms()
+        command = ""
+        for i in parms:
+            key = i['key']
+            value = i['value']
+            inObj = re.search(r'in=\[(.{5})\]*', value, re.M | re.I)
+            if inObj:
+                inputId = inObj.group(1)
+                input = func.getInput(inputId)
+                if input:
+                    input_path = input.getPath()
+                    if not input.getFrom():
+                        local_ipath = build_local_path(input_path, user_uuid)
+                    else:
+                        local_ipath = build_task_local_path(input_path, task_id, user_uuid)
+                    command = "{0} {1} {2}".format(command,key,local_ipath)
+            outObj = re.search(r'(\[out\](.*))', value, re.M | re.I)
+            if outObj:
+                output = func.getOutput()
+                output_path = output.getPath()
+                local_opath = build_task_local_path(output_path, task_id, user_uuid)
+                pat = re.compile(r'(\[out\])')
+                res = pat.sub(r'' +local_opath + '', value)
+                command = "{0} {1} {2}".format(command, key, res)
+            if not inObj and not outObj:
+                command = "{0} {1} {2}".format(command, key, value)
+
+        command = "{0} {1}".format("otbcli_EdgeExtraction",command)
+        doFunction(process, command)
+        # raster_stretch(local_ipath, local_opath)
     except Exception as e:
         logger.error("process run failed: {0}".format(str(e)))
         raise e
