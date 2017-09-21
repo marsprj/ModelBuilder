79d48b233f1cdcb90e0918c44f49c08aab006a07
fanzia <caisefan@163.com>
2017-09-21 01:21:27 -0700

    补充中值滤波算法

--- model/functions.py
+++ model/functions.py
@@ -261,6 +261,48 @@ def process_meanimagefilter(func, process, user_uuid):
     return True
 
 """
+处理中值滤波
+"""
+def process_medianimagefilter(func, process, user_uuid):
+    try:
+        task_id = str(process.task_id)
+        parms = func.getParms()
+        command = ""
+        for i in parms:
+            key = i['key']
+            value = i['value']
+            inObj = re.search(r'in=\[(.{5})\]*', value, re.M | re.I)
+            if inObj:
+                inputId = inObj.group(1)
+                input = func.getInput(inputId)
+                if input:
+                    input_path = input.getPath()
+                    if not input.getFrom():
+                        local_ipath = build_local_path(input_path, user_uuid)
+                    else:
+                        local_ipath = build_task_local_path(input_path, task_id, user_uuid)
+                    command = "{0} {1} {2}".format(command, key, local_ipath)
+            outObj = re.search(r'(\[out\](.*))', value, re.M | re.I)
+            if outObj:
+                output = func.getOutput()
+                output_path = output.getPath()
+                local_opath = build_task_local_path(output_path, task_id, user_uuid)
+                pat = re.compile(r'(\[out\])')
+                res = pat.sub(r'' + local_opath + '', value)
+                command = "{0} {1} {2}".format(command, key, res)
+
+
+        command = "{0} {1}".format("MedianImageFilter", command)
+        doFunction(process, command)
+    except Exception as e:
+        logger.error("process run failed: {0}".format(str(e)))
+        raise e
+        return False
+    return True
+
+
+
+"""
 处理平滑濾波
 """
 def process_smoothing(func, process, user_uuid):
