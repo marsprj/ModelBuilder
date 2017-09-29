83abc29bbb81951ab2422a7969a0b690b40440a9
fanzia <caisefan@163.com>
2017-09-29 09:59:36 +0800

    修改算法列表

--- static/js/funCatalog.js
+++ static/js/funCatalog.js
@@ -130,6 +130,14 @@ var g_funCatalog = [
  					}
  				]
  			},{
+ 				name : "道路提取",
+ 				items : [
+ 					{
+ 						name : "道路提取",
+ 						type : ""
+ 					}
+ 				]
+ 			},{
  				name : "云检测",
  				items:[
  					{
@@ -143,11 +151,56 @@ var g_funCatalog = [
 		name : "图像分割",
 		items:[
 			{
-				name : "连通域分割",
+				name : "区域生长",
+				items:[
+					{
+						name : "连通域分割",
+						type : ""
+					},{
+						name : "Otsu自动阈值分割",
+						type : "OtsuSegment"
+					}
+				]
+			},{
+				name : "基于分水岭的分割",
 				type : ""
 			},{
-				name : "Otsu自动阈值分割",
-				type : "OtsuSegment"
+				name : "水平集的图像分割",
+				type : ""
+			}
+		]
+	},{
+		name : "分类",
+		items:[
+			{
+				name : "非监督分类",
+				items : [
+					{
+						name : "K-Means分类",
+						type : "KMeans"
+					},{
+						name : "基于Kd树的KMeans聚类",
+						type : ""
+					},{
+						name : "SOM分类",
+						type : ""
+					},{
+						name : "贝叶斯分类",
+						type : ""
+					},{
+						name : "最大期望混合模型估计",
+						type : ""
+					},{
+						name : "统计分割",
+						type : ""
+					},{
+						name : "马尔科夫随机场分类",
+						type : ""
+					}
+				]
+			},{
+				name : "监督分类",
+				type : ""
 			}
 		]
 	},{
@@ -162,6 +215,66 @@ var g_funCatalog = [
 					},{
 						name : "Lee滤波",
 						type : ""
+					}
+				]
+			}
+		]
+	},{
+		name : "高光谱",
+		items :[
+			{
+				name : "Unmixing",
+				type : ""
+			},{
+				name : "降维",
+				type : ""
+			},{
+				name : "异常检测",
+				type : ""
+			}
+		]
+	},{
+		name : "变化检测",
+		items : [
+			{
+				name : "简单变化检测",
+				items : [
+					{
+						name : "平均变化检测",
+						type : "MeanDiffDetection"
+					},{
+						name : "平均比率变化检测",
+						type : "MeanRadioDiffDetection"
+					}
+				]
+			},{
+				name : "统计变化检测",
+				items : [
+					{
+						name : "基于Kullback-Leibler距离检测",
+						type : "KLDiffDetection"
+					},{
+						name : "基于相关系数的变化检测",
+						type : "CorrelationDiffDetection"
+					}
+				]
+			},{
+				name : "多尺度变化检测",
+				type : ""
+			},{
+				name : "多成分变化检测",
+				type : "MultivariateDiffDetection"
+			}
+		]
+	},{
+		name : "图像IO",
+		items : [
+			{
+				name : "影像裁剪",
+				items : [
+					{
+						name : "ROI裁剪",
+						type : "ROIExtract"
 					}
 				]
 			}
